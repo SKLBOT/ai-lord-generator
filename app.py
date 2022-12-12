d178434ed5c286e2f09069823c0fccd12a8467d1
SKLBOT <Ludeman911@gmail.com>
2022-12-11 23:56:30 -0500

    Remove old

--- app.py
+++ app.py
@@ -24,19 +24,6 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
-
-
 def generate_teammate_prompt(person):
     return """Suggest a funny name for a person as if they were a noble person from the middle ages.
 person: Sean
